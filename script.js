4931b94f3598027df8d992acc980f970c1c48974
Ana Castro de Araújo <anacabh@gmail.com>
2023-09-02 11:36:04 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+    alert('Selecione as Disciplinas');
+    console.log('your message');
+$('#mensagem').html('Escolha o que quer saber sobre cada Matéria!');
+
+    $('.btn-outline-success').click(function(){
+        alert("alerta de mensagem após o clic");
+    });
+
+});
