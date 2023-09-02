29833e17fd59cd11327bafed80afe0f13cb31483
Ana Castro de Araújo <anacabh@gmail.com>
2023-09-02 12:50:47 -0300

    textos

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 
 $(document).ready(function() {
     alert('Selecione as Disciplinas');
-    console.log('your message');
-$('#mensagem').html('Escolha o que quer saber sobre cada Matéria!');
+    console.log('Opa!');
+$('#mensagem').html('Escolha o que quer saber sobre cada matéria no final do segundo trimestre!');
 
-    $('.btn-outline-success').click(function(){
-        alert("alerta de mensagem após o clic");
-    });
+      $('#CN').click(function(){
+        alert("Em física estamos falando de cinemática, gases ideais e movimento através de fórmulas. Já em Biologia vimos sobre poluição e obtenção de energia, apresentando energias renováveis e práticas sustentáveis. Em química vimos Ligações químicas, mais especificamente sobre a ligação covalente e polaridade.");
+    }); 
+       $('#CH').click(function(){
+        alert("Estamos falando sobre as distribuição de terras, contextalizando através das capitanias hereditárias, documentos pós-colonização, grilagem e regiões.");
+    }); 
+       $('#MAT').click(function(){
+        alert("Falamos sobre função exponencial, sua funcionalidade e como calcular seus gráficos no excel e no geogebra.");
+    }); 
+       $('#LIG').click(function(){
+        alert("Em artes, estamos sendo contextualizados com conteúdos sobre roteiro, decupagem, edição, montagem, foley e trilha sonora, para contribuir para a produção de um curta proposto em aula. Em português vimos o conteúdo de entrevista, apresentando sua estrutura, exemplos e seus tipos. Já em inglês estavamos debatendo sobre o futuro e a tecnologia, recapitulando palavras e termos relacionados a tecnologia e ao mundo virtual. Em educação física vimos o módulo de danças e fomos avaliados através do desenvolvimento de uma coreografia registrada em formato de vídeo.");
+    }); 
+       $('#TEC').click(function(){
+        alert("Nos foi apresentado um framework que fornece estruturas HTML e CSS chamado Bootstrap, com isso estamos aprofundando nossos aprendizados.");
+    }); 
+       $('#PP').click(function(){
+        alert("Ultimamente recebemos a proposta de criarmos uma empresa fictícia com valores ESG, que se refere a práticas socialmente conscientes, sustentáveis e corretamente gerenciadas.");
+    }); 
+       $('#PV').click(function(){
+        alert("Com o objetivo de incentivar a busca e o interesse dos alunos, foi proposto uma dinâmica de sala de aula invertida, onde os alunos tem que produzir mini-aulas e apresenta-las em aula.");
+    }); 
+
+
+
 
 });
